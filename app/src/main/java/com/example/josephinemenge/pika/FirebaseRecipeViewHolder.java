ddafb45d9f396906d198f9f33263acc56540d132
Yonkokilasi <yonkokilasi@gmail.com>
2017-06-09 12:19:45 +0300

    Split the buttons on the main activity and added the view saved recipe's button and implemented a firebase adapter to bring back the data and a snapshot inline with an event listener that will update the data on change

--- app/src/main/java/com/example/josephinemenge/pika/FirebaseRecipeViewHolder.java
+++ app/src/main/java/com/example/josephinemenge/pika/FirebaseRecipeViewHolder.java
@@ -1,12 +1,24 @@
 package com.example.josephinemenge.pika;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.josephinemenge.pika.ui.RecipeDetailActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
 import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 /**
  * Created by Josephine Menge on 08/06/2017.
@@ -27,6 +39,32 @@ public class FirebaseRecipeViewHolder extends RecyclerView.ViewHolder implements
         TextView nameTextView = (TextView) mView.findViewById(R.id.recipeNameTextView);
         TextView sourceTextView = (TextView) mView.findViewById(R.id.recipeSource);
         TextView yieldTextView = (TextView) mView.findViewById(R.id.yieldTextView);
+        Picasso.with(mContext).load(recipe.getImageUrl()).centerCrop().into(recipeImageView);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        final ArrayList<Recipe> recipes = new ArrayList<>();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_RECIPES);
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    recipes.add(snapshot.getValue(Recipe.class));
+                }
+                int itemPostion = getLayoutPosition();
+                Intent intent = new Intent(mContext, RecipeDetailActivity.class);
+                intent.putExtra("position",itemPostion + "");
+                intent.putExtra("recipe", Parcels.wrap(recipes));
+                mContext.startActivity(intent);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
     }
 }
